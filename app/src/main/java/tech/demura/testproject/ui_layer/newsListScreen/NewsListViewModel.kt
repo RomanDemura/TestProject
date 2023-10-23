3f4b3107205a09a6e81a9d2c1b07aae083aec12d
Roman Demura <anokhin.roman.27@gmail.com>
2023-10-23 23:11:30 +1000

    add random generator

--- app/src/main/java/tech/demura/testproject/ui_layer/newsListScreen/NewsListViewModel.kt
+++ app/src/main/java/tech/demura/testproject/ui_layer/newsListScreen/NewsListViewModel.kt
@@ -1,11 +1,13 @@
 package tech.demura.testproject.ui_layer.newsListScreen
 
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import tech.demura.testproject.data_layer.NewsRepositoryImpl
 import tech.demura.testproject.domain_layer.news.entites.News
 import tech.demura.testproject.domain_layer.news.use_case.*
+
 
 class NewsListViewModel : ViewModel() {
 
@@ -44,6 +46,56 @@ class NewsListViewModel : ViewModel() {
     }
     fun markAllNews(){
         markAllNewsUseCase.markAllNews()
+    }
+
+    fun getPublishTime(news: News): String{
+        val currentTime = System.currentTimeMillis()
+        val publishTime = news.publishedDate
+        val diff = currentTime -publishTime
+        val minutes = diff / DateUtils.MINUTE_IN_MILLIS
+        val hours = diff / DateUtils.HOUR_IN_MILLIS
+        val days = diff / DateUtils.DAY_IN_MILLIS
+        val month = diff / (DateUtils.DAY_IN_MILLIS * 30)
+        val years = diff / (DateUtils.DAY_IN_MILLIS * 365)
+
+        val res = if (years >= 1){
+            val check = if (years == 1L){
+                "year"
+            } else{
+                "years"
+            }
+            "$years $check ago"
+        } else if (month >= 1){
+            val check = if (month == 1L){
+                "month"
+            } else{
+                "months"
+            }
+            "$month $check ago"
+        } else if (days >= 1){
+            val check = if (days == 1L){
+                "day"
+            } else{
+                "days"
+            }
+            "$days $check ago"
+        } else if (hours >= 1){
+            val check = if (hours == 1L){
+                "hour"
+            } else{
+                "hours"
+            }
+            "$hours $check ago"
+        } else if (minutes >= 1){
+            val check = if (minutes == 1L){
+                "minute"
+            } else{
+                "minutes"
+            }
+            "$minutes $check ago"
+        } else "Now"
+
+        return res
     }
 
 
